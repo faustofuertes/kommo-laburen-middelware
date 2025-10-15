88668a0c0b47afe2297fc438f3829410965c543d
faustofuertes <fausto.fuertes.83@gmail.com>
2025-10-15 17:20:45 -0300

    test whitelist

--- src/controllers/kommo.controller.js
+++ src/controllers/kommo.controller.js
@@ -22,7 +22,7 @@ export async function kommoWebhook(req, res) {
     const parsed = parseIncoming(raw, contentType);
     const normalized = normalizeIncomingMessage(parsed);
 
-    if (normalized.origin === 'waba' && normalized.element_id === '18694802') {
+    if (normalized.origin === 'waba' && normalized.element_id === '18712314') {
       console.log('Soy el faustito paaaa')
       //await processKommoMessage(normalized);
       console.log('--------------------------------------------------------------------------------------------------------------------------------------------------------');
