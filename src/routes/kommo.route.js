290c09cf654333f26b804ce38d54b49659d7dea1
faustofuertes <fausto.fuertes.83@gmail.com>
2025-10-16 11:13:20 -0300

    fix: import 2

--- src/routes/kommo.route.js
+++ src/routes/kommo.route.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { rawBody } from "../utils/rawBody.js";
-import { kommoMessageWebhook } from "../controllers/kommo.controller.js";
+import { kommoWebhook } from "../controllers/kommo.controller.js";
 const router = Router();
 
-router.post("/webhook", rawBody, kommoMessageWebhook);
+router.post("/webhook", rawBody, kommoWebhook);
 
 export default router;
