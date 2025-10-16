5b30bfeddfbcb4ce79cb76841479cdcf47d221dd
faustofuertes <fausto.fuertes.83@gmail.com>
2025-10-16 15:22:21 -0300

    test whitelist3

--- src/controllers/kommo.controller.js
+++ src/controllers/kommo.controller.js
@@ -6,7 +6,7 @@ import { sendWppMessage } from "../services/whatsapp.services.js";
 
 const idsPausados = new Set();
 const conversationMap = new Map();
-const whiteList = ['+5492233454259'];
+const whiteList = ['+5492233454259', '+5493548412165'];
 
 export async function kommoWebhook(req, res) {
   res.sendStatus(204); // responder rápido
@@ -24,7 +24,7 @@ export async function kommoWebhook(req, res) {
 
       const normalized = normalizeIncomingMessage(parsed);
       const contact = await getContact(normalized.contact_id);
-      if (normalized.origin === 'waba' && whiteList.has(contact.phone)) {
+      if (normalized.origin === 'waba' && whiteList.includes(contact.phone)) {
         await processKommoMessage(normalized);
       }
 
